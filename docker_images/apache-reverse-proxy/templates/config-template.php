37d3e63e6440aa11ad8cae09705091181f2af869
Def_Jamsky <aebischerle@gmail.com>
2018-05-13 15:25:42 +0200

    Dynamic ip reverse proxy

---
+++ docker_images/apache-reverse-proxy/templates/config-template.php
@@ -0,0 +1,16 @@
+<?php
+	$staticApp = getenv('STATIC_APP');
+	$dynamicApp = getenv('DYNAMIC_APP');
+?>
+
+<VirtualHost *:80>
+	ServerName demo.res.ch
+
+	ProxyPass "/api/animals/" "http://<?php print $dynamicApp ?>"
+	ProxyPassReverse "/api/animals/" "http://<?php print $dynamicApp ?>"
+
+	ProxyPass "/" "http://<?php print $staticApp ?>"
+	ProxyPassReverse "/" "http://<?php print $staticApp ?>"
+
+
+</VirtualHost>
